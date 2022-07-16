7a1016d83524c32f1277720111d33c704d6fc1f9
JAVHM <55992367+JAVHM@users.noreply.github.com>
2022-07-16 03:09:35 -0500

    .

--- app/src/main/java/pe/edu/ulima/circulima_v1/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/circulima_v1/MainActivity.kt
@@ -1,16 +1,14 @@
 package pe.edu.ulima.circulima_v1
 
 
-import android.content.Intent.EXTRA_TEXT
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import pe.edu.ulima.circulima_v1.R
 
 class MainActivity : AppCompatActivity(){
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_2)
+        setContentView(R.layout.main_activity)
 
         val cod = intent.getIntExtra("EXTRA_CODE", 0)
         val textView2 = findViewById(R.id.textview2) as TextView
